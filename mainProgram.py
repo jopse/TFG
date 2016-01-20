9dbd586e044694d02f0d7d6f4c7c1d1ce252d0ed
Jose Angel <jopse91@gmail.com>
2016-01-20 14:53:05 +0100

    Create mainProgram.py

---
+++ mainProgram.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 19/1/2016
+
+@author: Jose Angel Gonzalez Mejias
+'''
+import descarga as program
+
+def main():
+    program.main('CoG')
+    program.main('StG')
+    program.main('PoC')
+
+if __name__ == '__main__':
+    main()
