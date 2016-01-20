228e7b61d199b69ef95ea41f83fc117b220b7581
Jose Angel <jopse91@gmail.com>
2016-01-20 14:51:55 +0100

    Update descarga.py

--- descarga.py
+++ descarga.py
@@ -19,8 +19,11 @@ Redefinición de metodos para facilitar el parseado
 '''
 
 class MyHTMLParser(HTMLParser):
+    todosProyecto = []
     bodyData = dict()
     projects = False
+    esEtiqueta = False
+    saltarEtiqueta = False
     empiezaP = -1
     etiqueta = ""
     def handle_starttag(self, tag, attrs):
@@ -33,14 +36,32 @@ class MyHTMLParser(HTMLParser):
         if self.projects:
             if data == "Rcn":
                 self.empiezaP = 0
-            if self.empiezaP > 0 and self.empiezaP %2 == 0:
+                self.esEtiqueta = True
+                self.saltarEtiqueta = False
+            elif data == "Website (HI)":
+                self.empiezaP+=1
+                self.esEtiqueta = True
+                self.saltarEtiqueta = True
+            elif data == "Duration":
+                self.empiezaP+=1
+                self.esEtiqueta = True
+                self.saltarEtiqueta = True
+            else:
+                self.saltarEtiqueta = False
+            if self.empiezaP == 8 and data != "Researcher (PI)":
+                self.empiezaP+=2
+            if self.empiezaP >= 0 and self.esEtiqueta and not self.saltarEtiqueta:
                 self.etiqueta = data
                 self.empiezaP+=1
-            elif self.empiezaP > 0:
+                self.esEtiqueta = False
+            elif self.empiezaP >= 0 and not self.esEtiqueta and not self.saltarEtiqueta:
                 self.bodyData[self.etiqueta] = data
                 self.empiezaP+=1
-            if self.empiezaP > 17:
+                self.esEtiqueta = True
+            if self.empiezaP > 23:
                 self.empiezaP = -1
+                self.todosProyecto.append(self.bodyData)
+                self.bodyData = dict()
     def handle_comment(self, data):
         pass
     def handle_entityref(self, name):
@@ -54,88 +75,48 @@ class MyHTMLParser(HTMLParser):
         pass
 #Creacion del parser
 parser=MyHTMLParser()
-#Variable que contendra todo el codigo de todos los proyectos
-consolidatorGrantsProjects=""
 
+top = ET.Element("CGProjects")
 #Recorre todas las 62 paginas para extraer los proyectos solamente
-for i in range(1):
+f = open('hecho.html','a')
+for i in range(62):
     with request.urlopen(URL.format(ABBR['CoG'],'CoG',i)) as response:
         html = response.read()
         html = html.decode('utf-8')
-        f = open('hecho.html','w')
         f.write(html)
-        f.close()
         parser.feed(html)
-print(parser.bodyData)
-
-exit()
-#print("----------")
-#print(bodyData)
-
-# Coloca primero el acronimo del proyecto para seguir un orden logico
-#contador=1
-#for i in range(len(parser.bodyData)):
-#    if contador == 6:
-#        parser.bodyData.insert(i-5, parser.bodyData.pop(i))
-#        parser.bodyData.insert(i-5, parser.bodyData.pop(i))
-#    if "Rcn" in parser.bodyData[i]:
-#        contador=1
-#    contador+=1
-#print("----------")
-#print(bodyData)
-f = open('salida.xml','w')
-for i in range(len(parser.bodyData)):
-    f.write(parser.bodyData[i])
 f.close()
-
 #convierte a arbol xml los proyectos
-top = ET.Element("CGProjects")
-exit()
-for i in range(len(parser.bodyData)):
-    if(parser.bodyData[i] == "Project acronym"):
-        lastProject = ET.SubElement(top, "Project")
-        lastProject.attrib={"acronym":parser.bodyData[i+1]}
-    elif(parser.bodyData[i] == "Rcn"):
-        rcn = ET.SubElement(lastProject, "Rcn")
-        rcn.text = parser.bodyData[i+1]
-    elif(parser.bodyData[i] == "Nid"):
-        nid = ET.SubElement(lastProject, "Nid")
-        nid.text = parser.bodyData[i+1]
-    elif(parser.bodyData[i] == "Host Institution (HI)"):
-        hi = ET.SubElement(lastProject, "HI")
-        hi.text = parser.bodyData[i+1]
-    elif(parser.bodyData[i] == "Project"):
-        p = ET.SubElement(lastProject, "Name")
-        p.text = parser.bodyData[i+1]
-    elif(parser.bodyData[i] == "Researcher (PI)"):
-        pi = ET.SubElement(lastProject, "PI")
-        pi.text = parser.bodyData[i+1]
-    elif(parser.bodyData[i] == "Call details"):
-        cd = ET.SubElement(lastProject, "Call_Details")
-        cd.text = parser.bodyData[i+1]
-    elif(parser.bodyData[i] == "Summary"):
-        sm = ET.SubElement(lastProject, "Summary")
-        sm.text = parser.bodyData[i+1]
-    elif(parser.bodyData[i] == "Website (HI)"):
-        if(parser.bodyData[i+1] != "Max ERC funding"):
-            web = ET.SubElement(lastProject, "Web")
-            web.text = parser.bodyData[i+1]
-        else:
-            web = ET.SubElement(lastProject, "Web")
-            web.text = " "
-    elif(parser.bodyData[i] == "Max ERC funding"):
-        mef = ET.SubElement(lastProject, "Max_ERC_funding")
-        mef.text = parser.bodyData[i+1]
-    elif(parser.bodyData[i] == "Duration"):
-        dur = ET.SubElement(lastProject, "Duration")
-        sd = ET.SubElement(dur, "Start_date")
-        sd.text = parser.bodyData[i+2]
-        ed = ET.SubElement(dur, "End_date")
-        ed.text = parser.bodyData[i+4]
-        i+=4
+for j in range(len(parser.todosProyecto)):
+    proyectoAux = parser.todosProyecto[j]
+    lastProject = ET.SubElement(top, 'Project')
+    print(proyectoAux)
+    lastProject.attrib={'acronym':proyectoAux['Project acronym']}
+    rcn = ET.SubElement(lastProject, 'Rcn')
+    rcn.text = proyectoAux['Rcn']
+    nid = ET.SubElement(lastProject, 'Nid')
+    nid.text = proyectoAux['Nid']
+    hi = ET.SubElement(lastProject,'HI')
+    hi.text = proyectoAux['Host Institution (HI)']
+    p = ET.SubElement(lastProject,'Name')
+    p.text = proyectoAux['Project']
+    pi = ET.SubElement(lastProject,'PI')
+    if 'Researcher (PI)' in proyectoAux:
+        pi.text = proyectoAux['Researcher (PI)']
     else:
-        pass
-    i+=1
+        pi.text = " "
+    cd = ET.SubElement(lastProject,'Call_Details')
+    cd.text = proyectoAux['Call details']
+    sm = ET.SubElement(lastProject,'Summary')
+    sm.text = proyectoAux['Summary']
+    mef = ET.SubElement(lastProject,'Max_ERC_funding')
+    mef.text = proyectoAux['Max ERC funding']
+    dur = ET.SubElement(lastProject,'Duration')
+    sd = ET.SubElement(dur,'Start_date')
+    sd.text = proyectoAux['Start date:']
+    ed = ET.SubElement(dur, "End_date")
+    ed.text = proyectoAux['End date:']
+
 f = open('salida.xml','w')
 f.write('<?xml version="1.0" encoding="utf-8"?>')
 f.write('<!DOCTYPE CGProjects SYSTEM "validador.dtd">')
