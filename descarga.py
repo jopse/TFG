9027885e68c19f57ebbe0f3b4e35a98871327ec0
Jose Angel <jopse91@gmail.com>
2016-01-20 14:52:34 +0100

    Update descarga.py

--- descarga.py
+++ descarga.py
@@ -9,8 +9,8 @@ from html.entities import name2codepoint
 from html.parser import HTMLParser
 import xml.etree.cElementTree as ET
 
-
-URL = "http://erc.europa.eu/projects-and-results/erc-funded-projects?f[2]=sm_field_cordis_project_funding%3A{0!s}%20%28{1!s}%29&page={2!s}"
+SHORT_URL="/projects-and-results/erc-funded-projects?f[2]=sm_field_cordis_project_funding%3A{0!s}%20%28{1!s}%29"
+URL = "http://erc.europa.eu"+SHORT_URL+"&page={2!s}"
 ABBR = {'CoG':'Consolidator%20Grants','StG':'Starting%20Grant','PoC':'Proof%20of%20Concept'}
 
 
@@ -26,12 +26,18 @@ class MyHTMLParser(HTMLParser):
     saltarEtiqueta = False
     empiezaP = -1
     etiqueta = ""
+    pages = False
+    numPages = 0
     def handle_starttag(self, tag, attrs):
         if(tag == "ul"):
             self.projects = True
+        elif(tag == "a" and SHORT_URL.format(ABBR[projectType],projectType) in attrs):
+            self.pages = True
     def handle_endtag(self, tag):
         if(tag == "ul"):
             self.projects = False
+        elif(tag == "a"):
+            self.pages = False
     def handle_data(self, data):
         if self.projects:
             if data == "Rcn":
@@ -62,6 +68,8 @@ class MyHTMLParser(HTMLParser):
                 self.empiezaP = -1
                 self.todosProyecto.append(self.bodyData)
                 self.bodyData = dict()
+        elif self.pages:
+            self.numPages = data
     def handle_comment(self, data):
         pass
     def handle_entityref(self, name):
@@ -73,54 +81,75 @@ class MyHTMLParser(HTMLParser):
             c = int(name)
     def handle_decl(self, data):
         pass
-#Creacion del parser
-parser=MyHTMLParser()
 
-top = ET.Element("CGProjects")
-#Recorre todas las 62 paginas para extraer los proyectos solamente
-f = open('hecho.html','a')
-for i in range(62):
-    with request.urlopen(URL.format(ABBR['CoG'],'CoG',i)) as response:
+def main(argv):
+    global projectType
+    projectType = argv
+
+    #Creacion del parser
+    parser=MyHTMLParser()
+    parser2=MyHTMLParser()
+
+    top = ET.Element("{0}Projects".format(projectType))
+    #Recorre todas las paginas para extraer los proyectos
+    #f = open('salida_{0}.html'.format(projectType),'a')
+    pages = 0
+    with request.urlopen(URL.format(ABBR[projectType],projectType,0)) as response:
         html = response.read()
         html = html.decode('utf-8')
-        f.write(html)
-        parser.feed(html)
-f.close()
-#convierte a arbol xml los proyectos
-for j in range(len(parser.todosProyecto)):
-    proyectoAux = parser.todosProyecto[j]
-    lastProject = ET.SubElement(top, 'Project')
-    print(proyectoAux)
-    lastProject.attrib={'acronym':proyectoAux['Project acronym']}
-    rcn = ET.SubElement(lastProject, 'Rcn')
-    rcn.text = proyectoAux['Rcn']
-    nid = ET.SubElement(lastProject, 'Nid')
-    nid.text = proyectoAux['Nid']
-    hi = ET.SubElement(lastProject,'HI')
-    hi.text = proyectoAux['Host Institution (HI)']
-    p = ET.SubElement(lastProject,'Name')
-    p.text = proyectoAux['Project']
-    pi = ET.SubElement(lastProject,'PI')
-    if 'Researcher (PI)' in proyectoAux:
-        pi.text = proyectoAux['Researcher (PI)']
-    else:
-        pi.text = " "
-    cd = ET.SubElement(lastProject,'Call_Details')
-    cd.text = proyectoAux['Call details']
-    sm = ET.SubElement(lastProject,'Summary')
-    sm.text = proyectoAux['Summary']
-    mef = ET.SubElement(lastProject,'Max_ERC_funding')
-    mef.text = proyectoAux['Max ERC funding']
-    dur = ET.SubElement(lastProject,'Duration')
-    sd = ET.SubElement(dur,'Start_date')
-    sd.text = proyectoAux['Start date:']
-    ed = ET.SubElement(dur, "End_date")
-    ed.text = proyectoAux['End date:']
+        #f.write(html)
+        parser2.feed(html)
+        pages = parser2.numPages
+    #f.close()
+    #f = open('salida_{0}.html'.format(projectType),'a')
+    for i in range(pages):
+        with request.urlopen(URL.format(ABBR[projectType],projectType,i)) as response2:
+            html2 = response2.read()
+            html2 = html2.decode('utf-8')
+            #f.write(html)
+            parser.feed(html)
+        print("Page number {0} of {1} parsed".format(i,ABBR[projectType]))
+    #f.close()
+    #convierte a arbol xml los proyectos
+    for j in range(len(parser.todosProyecto)):
+        proyectoAux = parser.todosProyecto[j]
+        lastProject = ET.SubElement(top, 'Project')
+        print(proyectoAux)
+        try:
+            lastProject.attrib={'acronym':proyectoAux['Project acronym']}
+            rcn = ET.SubElement(lastProject, 'Rcn')
+            rcn.text = proyectoAux['Rcn']
+            nid = ET.SubElement(lastProject, 'Nid')
+            nid.text = proyectoAux['Nid']
+            hi = ET.SubElement(lastProject,'HI')
+            hi.text = proyectoAux['Host Institution (HI)']
+            p = ET.SubElement(lastProject,'Name')
+            p.text = proyectoAux['Project']
+            pi = ET.SubElement(lastProject,'PI')
+            if 'Researcher (PI)' in proyectoAux:
+                pi.text = proyectoAux['Researcher (PI)']
+            else:
+                pi.text = " "
+            cd = ET.SubElement(lastProject,'Call_Details')
+            cd.text = proyectoAux['Call details']
+            sm = ET.SubElement(lastProject,'Summary')
+            sm.text = proyectoAux['Summary']
+            mef = ET.SubElement(lastProject,'Max_ERC_funding')
+            mef.text = proyectoAux['Max ERC funding']
+            dur = ET.SubElement(lastProject,'Duration')
+            sd = ET.SubElement(dur,'Start_date')
+            sd.text = proyectoAux['Start date:']
+            ed = ET.SubElement(dur, "End_date")
+            ed.text = proyectoAux['End date:']
+        except KeyError:
+            continue
+    f = open('salida_{0}.xml'.format(projectType),'w')
+    f.write('<?xml version="1.0" encoding="utf-8"?>')
+    f.write('<!DOCTYPE {0}Projects SYSTEM "validador.dtd">'.format(projectType))
+    f.close()
+    # excritura en un fichero xml del arbol
+    tree = ET.ElementTree(top)
+    tree.write('salida_{0}.xml'.format(projectType))
 
-f = open('salida.xml','w')
-f.write('<?xml version="1.0" encoding="utf-8"?>')
-f.write('<!DOCTYPE CGProjects SYSTEM "validador.dtd">')
-f.close()
-# excritura en un fichero xml del arbol
-tree = ET.ElementTree(top)
-tree.write("salida.xml")
+if __name__ == "__main__":
+    main(sys.argv)
